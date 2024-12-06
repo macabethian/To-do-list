200d798b913372dcaed988a35cf1dae6efd00ff5
macabethian <vikatth@gmail.com>
2024-12-06 17:58:31 -0300

    Implementada funcionalidade de adicionar tarefa

---
+++ app.js
@@ -0,0 +1,11 @@
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+
+    if (taskInput.value.trim() !== '') {
+        const newTask = document.createElement('li');
+        newTask.textContent = taskInput.value;
+        taskList.appendChild(newTask);
+        taskInput.value = ''; // Limpa o campo de input após adicionar a tarefa
+    }
+}
